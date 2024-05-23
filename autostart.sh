67aa7c8ab44f4fff27979b44f18e7d54ed002332
Raunak Wete <raunakwete43@gmail.com>
2024-05-23 19:04:57 +0530

    new

--- autostart.sh
+++ autostart.sh
@@ -9,9 +9,11 @@ xinput disable "ELAN1301:00 04F3:30EF Touchpad" &
 
 # custom scripts
 bash ~/Downloads/scripts/headphone.sh &
+bash ~/Downloads/scripts/lock-screen.sh &
 
 # volumeicon &
-xss-lock --transfer-sleep-lock -- /bin/betterlockscreen -l &
+xss-lock --transfer-sleep-lock -- betterlockscreen -l &
 sxhkd -c ~/.config/qtile/sxhkdrc &
 
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/dunst -conf ~/.config/dunstrc &
